88e9a01fcd42623d299ced6c43ff8739dd9ea28b
LitenApe <thanhsonvo95@gmail.com>
2016-09-28 16:00:24 +0200

    hmm

--- parser/WhileStatm.java
+++ parser/WhileStatm.java
@@ -1,5 +1,38 @@
+package parser;
+import main.*;
+import scanner.*;
+import static scanner.TokenKind.*;
+
 public class WhileStatm extends Statement{
-    public WhileStatm(){
-        super();
+    Expression expr;
+    Statement body;
+
+    public WhileStatm(int lNum){
+        super(lNum);
     } /* End of constructor */
+
+    @Override public String identify() {
+        return "<while-statm> on line " + lineNum;
+    }
+
+    @Override void prettyPrint() {
+        Main.log.prettyPrint("while "); expr.prettyPrint();
+        Main.log.prettyPrintLn(" do"); Main.log.prettyIndent();
+        body.prettyPrint(); Main.log.prettyOutdent();
+    }
+
+    static WhileStatm parse(Scanner s) {
+        enterParser("while-statm");
+
+        WhileStatm ws = new WhileStatm(s.curLineNum());
+        s.skip(whileToken);
+
+        ws.expr = Expression.parse(s);
+        s.skip(doToken);
+        ws.body = Statement.parse(s);
+
+        leaveParser("while-statm");
+        return ws;
+    }
+
 } /* End of class */
