9c253346cc4bd922451bf277d1019481835ab960
LitenApe <thanhsonvo95@gmail.com>
2016-09-28 15:30:57 +0200

    hehe

--- parser/Program.java
+++ parser/Program.java
@@ -1,9 +1,14 @@
 public class Program extends PascalDecl{
     Block proBlock;
+    Scanner s;
 
     public Program(String id, int lNum){
         super(String id, int lNum);
     }/*Enc constructor*/
+
+    public parse(Scanner s){
+        this.s = s;
+    }
 
     @Override public String identify() {
         return "<program> " + name + " on line " + lineNum;
