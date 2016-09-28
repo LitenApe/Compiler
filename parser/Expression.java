968c803a0bd961ba3b354184cef74ef777d0fcb8
LitenApe <thanhsonvo95@gmail.com>
2016-09-28 13:06:52 +0200

    Forgot one conflict

--- parser/Expression.java
+++ parser/Expression.java
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-Class Expression{
-  
-}
-=======
 public class Expression{
 
     public Expression(){
 
     }/*End constructor*/
 }/*End class*/
->>>>>>> a46a6d34479ca475e2d13ad86dfc006b66924608
