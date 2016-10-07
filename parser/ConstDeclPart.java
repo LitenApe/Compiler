88c08e948db86f83bfd8c36cc76fa333d99933e0
Sharu K <sharanak@uio.no>
2016-10-07 23:35:09 +0200

    Fix ConstDeclPart.java for multiple ConstDecl's

--- parser/ConstDeclPart.java
+++ parser/ConstDeclPart.java
@@ -5,7 +5,8 @@ import static scanner.TokenKind.*;
 
 public class ConstDeclPart extends PascalSyntax{
 
-    ConstDecl constDecl;
+    // ArrayList<ConstDecl> constDeclarations = new ArrayList<>();
+    ConstDecl constDecl = null;
     public ConstDeclPart(int n){
         super(n);
     }/*End constructor*/
@@ -15,9 +16,13 @@ public class ConstDeclPart extends PascalSyntax{
 
         s.skip(constToken);
         ConstDeclPart constDeclPart = new ConstDeclPart(s.curLineNum());
-
         constDeclPart.constDecl = ConstDecl.parse(s);
-        s.skip(constToken);
+        // while(s.curToken.kind != semicolonToken){
+            // ConstDecl constDecl = ConstDecl.parse(s);
+            // constDeclarations.add(constDecl);
+        // }
+        //TODO: Const Decl Part should have many ConstDecls. What to do here?
+        // s.skip(constToken);
 
         leaveParser("const decl part");
         return constDeclPart;
