2e131158840ed6eed083474c8a38e7b1bfa525ad
Sharu95 <sharanak@uio.no>
2016-10-12 22:00:10 +0200

    go at it m8

--- parser/ProcDecl.java
+++ parser/ProcDecl.java
@@ -2,12 +2,13 @@ package parser;
 
 import scanner.Scanner;
 import static scanner.TokenKind.*;
+import main.Main;
 
 public class ProcDecl extends PascalDecl{
 
     // proc : name : param decl list : ; : block : ;
 
-    public String procName = null;
+    public NamedConst procName = null;
     public ParamDeclList paramDcl = null;
     public Block blk = null;
 
@@ -15,16 +16,28 @@ public class ProcDecl extends PascalDecl{
         super(id, lNum);
     }/*Enc constructor*/
 
+    @Override
+    public void prettyPrint(){
+        Main.log.prettyPrint("procedure ");
+        procName.prettyPrint();
+
+        if (paramDcl != null){
+            paramDcl.prettyPrint();
+        }
+
+        Main.log.prettyPrintLn(";");
+        blk.prettyPrint();
+        Main.log.prettyPrintLn(";");
+
+    }/*End prettyPrint*/
+
     public static ProcDecl parse(Scanner s){
         enterParser("proc decl");
 
         ProcDecl pDcl = new ProcDecl(s.curToken.id, s.curLineNum());
 
         s.skip(procedureToken);
-        s.test(nameToken);
-
-        pDcl.procName = s.curToken.id;
-        s.skip(nameToken);
+        pDcl.procName = NamedConst.parse(s);
 
         if(s.curToken.kind == leftParToken){
             pDcl.paramDcl = ParamDeclList.parse(s);
@@ -63,9 +76,4 @@ public class ProcDecl extends PascalDecl{
     public void checkWhetherValue(PascalSyntax where){
 
     }/*End checkWhetherValue*/
-
-    @Override
-    public void prettyPrint(){
-
-    }/*End prettyPrint*/
 }/*End class*/
