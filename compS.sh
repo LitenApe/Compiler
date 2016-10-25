4b94caf84012407938f41fd4c6827552df520955
LitenApe <thanhsonvo95@gmail.com>
2016-10-25 10:13:29 +0200

    scripts

---
+++ compS.sh
@@ -0,0 +1,57 @@
+# get current path
+curPath=$(pwd)
+# set path for directories
+resPath="$curPath/pascal/resLogs/scannerLog/"
+fasPath="$curPath/pascal/testFF/scanner/"
+
+# scanner
+# retrieve log files
+resultater=$(ls $resPath)
+fasit=$(ls $fasPath)
+
+# make it into an array
+resultater=$resultater | cut -d ' ' -f 1,2,3,4,5,6
+fasit=$fasit | cut -d ' ' -f 1,2,3,4,5,6
+
+# iterate through the files
+echo ""
+echo "TESTING SCANNER LOGS"
+for fas in $fasit; do
+  for res in $resultater; do
+    if [[ $res == $fas ]]; then
+      tester="$(diff -b "$resPath$res" "$fasPath$fas")"
+      if [[ ${#tester} -eq 0 ]]; then
+        echo "Passed:" $fas
+      else
+        echo "Failed:" $fas
+      fi
+    fi
+  done
+done
+
+resPath="$curPath/pascal/resLogs/parserLog/"
+fasPath="$curPath/pascal/testFF/parser/"
+
+# scanner
+# retrieve log files
+resultater=$(ls $resPath)
+fasit=$(ls $fasPath)
+
+# make it into an array
+resultater=$resultater | cut -d ' ' -f 1,2,3,4,5,6
+fasit=$fasit | cut -d ' ' -f 1,2,3,4,5,6
+
+echo ""
+echo "TESTING PARSER LOGS"
+for fas in $fasit; do
+  for res in $resultater; do
+    if [[ $res == $fas ]]; then
+      tester="$(diff -b "$resPath$res" "$fasPath$fas")"
+      if [[ ${#tester} -eq 0 ]]; then
+        echo "Passed:" $fas
+      else
+        echo "Failed:" $fas
+      fi
+    fi
+  done
+done
