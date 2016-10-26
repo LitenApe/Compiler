d5671db36a85059527182ebe5db688193ddf563e
LitenApe <thanhsonvo95@gmail.com>
2016-10-26 12:29:09 +0200

    Becaues, f u

--- parser/Library.java
+++ parser/Library.java
@@ -7,35 +7,24 @@ import java.util.ArrayList;
 public class Library extends Block{
 
     // store everything that is created for this libaries scope
-    public static HashMap<String, PascalDecl> declarations = null;   // Happy ? "YES I AM :D:D:D" : "Readable name";
+    public static HashMap<String, PascalDecl> declarations = new HashMap<>();   // Happy ? "YES I AM :D:D:D" : "Readable name";
 
     public Library(int lineNum){
         super(lineNum);
-        declarations = new HashMap<>();
     }/*End of constructor*/
 
     @Override
     public PascalDecl findDecl(String id, PascalSyntax where){
         System.out.println("2. MADYAR: AKE: "+id);
         PascalDecl found = declarations.get(id);
-        if (found != null){
+
+        if (found != null)
             Main.log.noteBinding(id,where,found);
-            return found;
-        }
-        where.error("Name " + id + " is unknown!");
-        return null;
-    }
 
-    // public void addDecl(PascalDecl decls){
-    //     String name = decls.toString();  // this variable is for readability.
-    //
-    //     if(declarations.containsKey(name))
-    //         Main.error(name + " is declared twice in the same block");
-    //
-    //     declarations.put(name, decls);
-    // }/*End of addConstants*/
+        if(found == nul)
+            where.error("Name " + id + " is unknown!");
 
-    // public PascalDecl findDecl(String name){
-    //     return declarations.get(name);
-    // }/*End of findDeclarations*/
+        return found;
+    }/*End of findDecl*/
+
 }/*End of class*/
