bd22eff47b1c56fbb04215de546976e36af52a9e
LitenApe <thanhsonvo95@gmail.com>
2016-10-27 16:22:48 +0200

    im first

--- types/Type.java
+++ types/Type.java
@@ -8,9 +8,9 @@ public abstract class Type {
     public abstract String identify();
 
     public void checkType(Type tx, String op, PascalSyntax where, String mess) {
-	Main.log.noteTypeCheck(this, op, tx, where);
-	if (this != tx)
-	    where.error(mess);
+    	Main.log.noteTypeCheck(this, op, tx, where);
+    	if (this != tx)
+    	    where.error(mess);
     }
 
     public static Type parse(Scanner s){
